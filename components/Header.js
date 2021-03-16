5f33d7058ff19ae0b08f8e5102104e2c57ecc52d
OnceLearner <redaamir972@gmail.com>
2021-03-16 19:10:46 +0100

    featue

--- components/Header.js
+++ components/Header.js
@@ -5,14 +5,17 @@ const Header = () => {
     return (
         <div className="w-full h-screen" >
 
-            <div className="relative w-full bg-black z-0 " style={{ height: "70vh" }}>
+            <div className="relative w-full bg-black z-0 flex flex-col items-center " style={{ height: "70vh" }}>
                 <Image src="/Carousel2.jpg" layout="fill" className={`transform  hover:scale-105 ransition-transform duration-700 ease-in`} objectFit="cover" priority="true" />
-                <div className="absolute top-0  transition-opacity duration-300 ease-linear  right-0 left-0 bottom-0 bg-gradient-to-br from-black to-transparent z-10 opacity-40    "></div>}
+                <div className="absolute top-0  transition-opacity duration-300 ease-linear  right-0 left-0 bottom-0 bg-gradient-to-br from-black to-transparent z-10 opacity-40    "></div>
+
+                <div className=" relative top-2/3    w-5/6      z-20                   bg-white rounded  shadow-lg  md:p-5  flex flex-col   justify-evenly" >
+                    <p className="text-gray-800">hh</p>
+                </div>
+
             </div>
 
-            <div className=" relative top-3/4   z-20 hidden   p-8       w-full   h-32      bg-white rounded-xl  shadow-lg  md:p-16  flex flex-col   justify-evenly" >
 
-            </div>
 
 
         </div>
